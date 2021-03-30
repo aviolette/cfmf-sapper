3b472b40dc1fdc8e92c5337f40be3f0837f2ddfc
Andrew Violette <aviolette@divvydose.com>
2021-03-30 07:31:43 -0500

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,34 @@
+
+
+<svelte:head>
+	<title>Chicago Farmers Market Finder</title>
+</svelte:head>
+
+<h1>Chicago Farmers' Market Finder</h1>
+
+<script>
+	import Box from '../components/Box.svelte';
+
+	let today = new Date().toLocaleDateString("en-US")
+	async function getData() {
+		const response = await fetch("/foobar.json")
+		return await response.json()
+	}
+	let marketPromise = getData()
+</script>
+
+<h2>Markets Open Today - {today}</h2>
+
+{#await marketPromise}
+{:then markets}
+	{#each markets["data"] as market}
+		<Box>
+			{market["name"]}<br/>
+			7am-1:30pm<br/>
+			1234 N. Baz Bar Lane,<br/>
+			Schaumburg, IL<br/>
+
+		</Box>
+	{/each}
+{/await}
+
