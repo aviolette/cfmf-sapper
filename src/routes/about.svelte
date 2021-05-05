ae1a751e8d617aca905941f03358f2c00b3d0ca0
Andrew Violette <andrew@andrewviolette.net>
2021-05-05 09:44:38 -0500

    Updated about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,4 +4,6 @@
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>Welcome to the Chicago Farmers' Market Finder!  This site displays a map with the farmers' markets that are open today.  If you find the information on this website  to be incorrect or incomplete, drop me a line at <a href="mailto:andrew+cfmf@andrewviolette.net">andrew+cfmf@andrewviolette.net</a>.</p>
+
+<p><i>Disclaimer: I am not affiliated in any way with any farmers markets.</i></p>
